3c366a632c4802990e313c85388f92a8ec6c9309
Ryan Regalado <ryanreg48@gmail.com>
2021-12-25 18:20:55 -0800

    setup query function

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import RepoInfo from './RepoInfo'
 function App() {
   let [userName, setUserName] = useState('');
   let [repoList, setRepoList] = useState(null);
+  let [pageCount, setPageCount] = useState(10);
+  let [queryString, setQueryString] = useState('javascript');
+  let [totalCount, setTotalCount] = useState(null);
   
   const fetchData = useCallback(() => {
+    const queryText = JSON.stringify(query(pageCount, queryString))
+
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(query)
+      body: queryText
     })
     .then(response => {
       return response.json()
@@ -19,11 +24,14 @@ function App() {
     .then(data => {
       const viewer = data.data.viewer
       const repos = data.data.search.nodes
+      const total = data.data.search.repositoryCount
       setUserName(viewer.name);
       setRepoList(repos);
+      setTotalCount(total)
+
     })
     .catch(err => console.log(err))
-  }, [])
+  }, [pageCount, queryString])
 
   useEffect(() => {
     fetchData();
@@ -33,6 +41,9 @@ function App() {
     <div className="App container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> Repos</h1>
       <p>Hey there {userName}</p>
+      <p>
+        <b>Search for: </b> {queryString} |  <b>Items per page: </b> {pageCount} |  <b>Total results: </b> {totalCount} 
+      </p>
       {
         repoList && (
           <ul className="list-group list-group-flush">
